3caf88dbef60aeb042205bf16ed011d6db10db46
KCY <kouchengyi@qiyi.com>
2019-12-29 14:36:22 +0800

    commit practise git

---
+++ PractiseGit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PractiseGit
+//
+//  Created by 寇诚意 on 2019/12/29.
+//  Copyright © 2019 寇诚意. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ PractiseGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PractiseGit
+//
+//  Created by 寇诚意 on 2019/12/29.
+//  Copyright © 2019 寇诚意. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
